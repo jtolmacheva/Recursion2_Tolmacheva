0b7bf5f13b2ff9017a206e27d1011cfab7cd6fb8
tolmacheva <tolmacheva.july920506@gmail.com>
2024-05-28 19:33:15 +0300

    Add task1

---
+++ task1/Program.cs
@@ -0,0 +1,33 @@
+﻿/*Задача 1: Задайте значения M и N. Напишите 
+программу, которая выведет все натуральные числа 
+в промежутке от M до N. Использовать рекурсию, не 
+использовать циклы.
+
+M = 1; N = 5 -> "1, 2, 3, 4, 5" 
+M = 4; N = 8 -> "4, 5, 6, 7, 8"*/
+
+
+
+void OneToN(int n, int m)
+{
+    
+    if (m > n)
+    {
+        OneToN(m - 1, n);
+        System.Console.Write(m + " ");
+    }
+    else if (n > m)
+    {
+        OneToN(n - 1, m);
+        System.Console.Write(n + " ");
+    }
+    else if (m == n)
+    {
+        System.Console.Write(m + " ");
+
+    }
+    
+    
+}
+OneToN(4, 8);
+
